7677be0fc637ad1851ba61d213fe31a97f0774e3
felipetexa <felipe.teixeira91@hotmail.com>
2021-11-28 18:58:46 -0300

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Title} from './components/title'
+
 
 function App() {
+
+  const [subtext, setSubtext] = useState();
+
   return (
     <div className="App">
       <header className="App-header">
+        <Title subtext={subtext}/>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="text"></input>
+        <button onClick={() => setSubtext("Gasto inserido")}>INSERIR GASTO</button>
       </header>
     </div>
   );
---
+++ src/components/title.jsx
@@ -0,0 +1,6 @@
+export const Title = ({subtext}) => (
+    <div>
+      <h1>Finanças para casais</h1>
+      <h2>{subtext}</h2>
+    </div>
+  );
